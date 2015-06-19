9db432a8dcfa787902c864b9180a78cc654850d7
zyxnice <zyxnice@zyxdeMacBook-Pro.local>
2015-06-20 01:43:08 +0800

    Initial Commit

---
+++ xyapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xyapp
+//
+//  Created by zyxnice on 15/6/20.
+//  Copyright (c) 2015年 hj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
