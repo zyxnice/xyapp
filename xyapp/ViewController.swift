d8dcebd718e7a180bcba83a41bd28cb9f560a318
zyx <zyxnice@aliyun.com>
2015-09-10 17:05:16 +0800

    初始版本

--- xyapp/ViewController.swift
+++ xyapp/ViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webview:UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        webview.scrollView.bounces = false
+        loadUrl("http://www.xyeach.com:5535")
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func loadUrl(url:String)
+    {
+        var urlobj = NSURL(string:url)
+        
+        var request = NSURLRequest(URL:urlobj!)
+        
+        webview.loadRequest(request);
     }
 
 
